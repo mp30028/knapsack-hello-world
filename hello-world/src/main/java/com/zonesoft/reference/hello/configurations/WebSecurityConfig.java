38cd38c0367c77a3fd0baab120149d808185d807
mp30028 <mp30028@gmail.com>
2023-05-15 08:34:17 +0100

    fix failing unit test by creating test profile properties

--- hello-world/src/main/java/com/zonesoft/reference/hello/configurations/WebSecurityConfig.java
+++ hello-world/src/main/java/com/zonesoft/reference/hello/configurations/WebSecurityConfig.java
@@ -1,16 +1,18 @@
 package com.zonesoft.reference.hello.configurations;
 
-
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
 @EnableWebSecurity
+@ConditionalOnProperty(name = "keycloak.enabled", havingValue = "true", matchIfMissing = true)
 public class WebSecurityConfig {
-	
+	    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests()
